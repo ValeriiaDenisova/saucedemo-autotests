8edad417d7f73da7516ed053bca89e110daa07fb
vdenysova <dmytrijevavi@gmail.com>
2025-08-13 23:47:54 +0300

    refactored code after feedback

--- test/specs/_9_checkoutWithoutProducts.e2e.ts
+++ test/specs/_9_checkoutWithoutProducts.e2e.ts
@@ -1,6 +1,8 @@
 import LoginPage from '../pageobjects/login.page'
 import InventoryPage from '../pageobjects/inventory.page'
 import CartPage from '../pageobjects/cart.page'
+import CheckoutPage from '../pageobjects/checkout.page'
+
 import { testUsers } from '../config/testUsers'
 import { baseTestSetup } from './baseTest/baseTestSetup'
 
@@ -31,10 +33,8 @@ describe('Test Case #9: Checkout without products', () => {
 
         const currentUrl = await CartPage.getCurrentUrl()
         expect(currentUrl).toContain('/cart.html')
-
-        const checkoutPage = (await import('../pageobjects/checkout.page')).default
         
-        const errorMessage = await checkoutPage.getErrorMessage()
+        const errorMessage = await CheckoutPage.getErrorMessage()
         expect(errorMessage).toContain('Cart is empty')
     
     })
